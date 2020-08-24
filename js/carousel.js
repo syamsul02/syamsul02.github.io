9f026aa4c26a3ce76087db0d028af35ab2a6030e
syamsul02 <69294916+syamsul02@users.noreply.github.com>
2020-08-24 09:48:47 +0700

    Add files via upload

---
+++ js/carousel.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    var getActiveSlideIndex = function () {
+      return $("#productSlider .active").index("#productSlider .carousel-item");
+    };
+    var slider = function (IndexNumber) {
+      $(".slider .image")
+        .removeClass("active")
+        .eq(IndexNumber)
+        .addClass("active");
+    };
+    slider(getActiveSlideIndex());
+    $("#productSlider").on("slid.bs.carousel", function () {
+      slider(getActiveSlideIndex());
+    });
+  });
+  
